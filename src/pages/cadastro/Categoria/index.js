02485ff5b4dde0e2ab9b78b9ebf5a61cee6cdb54
João Victor <j2003.victir@gmail.com>
2020-08-03 00:39:52 -0300

    serverr

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -28,7 +28,9 @@ function CadastroCategoria() {
   }
 
   useEffect(() => {
-    const URL = 'http://localhost:8080/categorias';
+    const URL = window.location.hostname.includes('localhost') 
+    ? 'https://localhost:8080/categorias'
+    : 'https://codeflixx.herokuapp.com/categorias';
 
     fetch(URL)
       .then(async (respostaDoServidor) => {
